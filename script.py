40cae9c842105685dab06b14a1c86447da769a1b
YijieGu <73412566+YijieGu@users.noreply.github.com>
2022-01-11 21:42:32 -0700

    new version of Python script

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests
 print(requests.__version__)
-homepage=requests.get("http://google.com")
+#homepage=requests.get("http://google.com")
+homepage= requests.get("https://raw.githubusercontent.com/YijieGu/404lab1/main/script.py")
 print(homepage.text)
