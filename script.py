2a7ef711681870dc53ac9bc3e69180426cb04e1e
YijieGu <73412566+YijieGu@users.noreply.github.com>
2022-01-14 13:23:55 -0700

    Change

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import requests
 print(requests.__version__)
-#homepage=requests.get("http://google.com")
-homepage= requests.get("https://raw.githubusercontent.com/YijieGu/404lab1/main/script.py")
+homepage=requests.get("http://www.google.com")
 print(homepage.text)
+new_version= requests.get("https://raw.githubusercontent.com/YijieGu/404lab1/main/script.py")
+print(new_version.text)
